cf233b087e223f732ff736f828a64995bb791525
zhshakib <zhshakib@gmail.com>
2024-11-14 23:17:54 +0600

    chore: About  us Button added

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="customizeNav navbar bg-base-100">
-        <a class="btn btn-ghost text-2xl">
-            <span class="text-orange-500">UGV</span> ShowCase
-        </a>
-        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto md:mr-5" />
-        <a class="btn btn-ghost">
-            <Icon class="mr-2" name="mdi:account"></Icon>
-            <span class="hidden md:inline">About Us</span>
-        </a>
-
-    </div>
+    <header class="bg-base-100">
+        <div class="customizeNav navbar bg-base-100">
+            <a class="btn btn-ghost text-2xl flex items-center justify-center">
+                <span class="text-orange-500">UGV</span> ShowCase
+            </a>
+            <input type="text" placeholder="Search for tags!" class="input input-bordered w-24 md:w-1/2 md:mr-5" />
+            <a class="btn hover:bg-orange-800 btn-ghost text-lg">
+                About Us
+            </a>
+        </div>
+    </header>
 </template>
 
 <style>
 .customizeNav {
+    max-width: 80%;
+    margin: 0px auto;
+    padding: 1rem 1rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
