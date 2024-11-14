9baf21d02c86498561b0c3fcce75b9b90af91495
zhshakib <zhshakib@gmail.com>
2024-11-14 22:25:58 +0600

    chore: Navbar Design Complete

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+import Navbar from './Navbar.vue';
+</script>
+
 <template>
-    <header class="px-10 py-5 bg-black text-white border-b-2 ">
-        <h1 class="text-2xl text-center ">
-            3rd Semester Routine
-        </h1>
-    </header>
+    <Navbar />
 </template>
 
 <style lang="css">
---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="customizeNav navbar bg-base-100">
+        <a class="btn btn-ghost text-2xl">
+            <span class="text-orange-500">UGV</span> ShowCase
+        </a>
+        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto md:mr-5" />
+        <a class="btn btn-ghost">
+            <Icon class="mr-2" name="mdi:account"></Icon>
+            <span class="hidden md:inline">About Us</span>
+        </a>
+
+    </div>
+</template>
+
+<style>
+.customizeNav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
