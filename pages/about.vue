3b60da3b2de85cc46b279dfa0fc2a641a6f6cd40
zhshakib <zhshakib@gmail.com>
2024-11-16 01:39:24 +0600

    chore: Team Sections Added!

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,52 @@
 <template>
-    <h1 class="text-2xl font-bold text-center mt-5">This is About Page</h1>
+    <header class="bg-base-100">
+        <h1 class="text-3xl font-medium text-center py-5">
+            <a href="/">
+                <span class="text-orange-500">Swift_</span><span class="text-red-500">Code</span>
+            </a>
+        </h1>
+    </header>
+    <main>
+        <section class="w-full mx-auto my-5">
+            <h2 class="text-2xl font-medium text-center mb-2">
+                Meet Our <span class="text-orange-500">Team</span>
+            </h2>
+        </section>
+        <section class="w-full mx-auto bg-base-100 p-5">
+            <div class="lg:w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-auto mx-5 gap-5">
+                <div class="flex flex-col items-center justify-center gap-2 bg-base-300 p-5 rounded-2xl">
+                    <NuxtImg src="/img/Nazia.jpg" alt="Nazia Islam" width="300" height="300" class="rounded-3xl mb-2" />
+
+                    <h2 class="text-2xl font-bold text-center mb-2">Nazia Islam</h2>
+                    <p class="text-sm font-medium text-center mb-2">Creative Designer</p>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-2 bg-base-300 p-5 rounded-2xl">
+                    <NuxtImg src="/img/Jarin.jpg" width="300" height="300" alt="Nazia Islam" class="rounded-3xl mb-2" />
+                    <h2 class="text-2xl font-bold text-center mb-2">Jarin Rahman</h2>
+                    <p class="text-sm font-medium text-center mb-2">Creative Designer</p>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-2 bg-base-300 p-5 rounded-2xl">
+                    <NuxtImg src="/img/Sumaiya.jpg" alt="Nazia Islam" width="300" height="300"
+                        class="rounded-3xl mb-2" />
+
+                    <h2 class="text-2xl font-bold text-center mb-2">Sumaiya Jannat</h2>
+                    <p class="text-sm font-medium text-center mb-2">Web Designer</p>
+                </div>
+            </div>
+            <div class="lg:w-1/2 grid grid-cols-1 md:grid-cols-2  lg:mx-auto mx-5 gap-5 mt-5">
+                <div class="flex flex-col items-center justify-center gap-2 bg-base-300 p-5 rounded-2xl">
+                    <NuxtImg src="/img/Shakib.jpg" width="300px" height="300px" alt="Nazia Islam"
+                        class="rounded-3xl mb-2" />
+                    <h2 class="text-2xl font-bold text-center mb-2">ZH Shakib</h2>
+                    <p class="text-sm font-medium text-center mb-2">Developer</p>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-2 bg-base-300 p-5 rounded-2xl">
+                    <NuxtImg src="/img/Soyeb.jpg" width="300px" height="300px" alt="Nazia Islam"
+                        class="rounded-3xl mb-2" />
+                    <h2 class="text-2xl font-bold text-center mb-2">Soyeb Mahmud</h2>
+                    <p class="text-sm font-medium text-center mb-2">Developer</p>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
